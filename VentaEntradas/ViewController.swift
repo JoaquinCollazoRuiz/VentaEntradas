b253d24cde69b8641c1f38e2a8df6124397a1481
JoaquinCollazoRuiz <joaquin_collazo_apps1ma1718@cev.com>
2018-12-04 17:13:18 +0100

    Initial Commit

---
+++ VentaEntradas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VentaEntradas
+//
+//  Created by Joaquín Collazo Ruiz on 04/12/2018.
+//  Copyright © 2018 Joaquín Collazo Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
